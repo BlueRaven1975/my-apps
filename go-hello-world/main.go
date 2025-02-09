e92da23d9cf1a44ffe26d099cda7f5f9579b69c8
Romano Romano <romano.romano@gmail.com>
2025-02-09 11:06:03 +0100

    chore: release go-hello-world application

---
+++ go-hello-world/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"rsc.io/quote"
+)
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func quoteHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, quote.Go())
+}
+
+func main() {
+	http.HandleFunc("/", helloWorldHandler)
+	http.HandleFunc("/quote", quoteHandler)
+
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
