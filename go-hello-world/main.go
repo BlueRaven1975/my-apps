53d815535617e16d458894aae1db187029b37d2b
Romano Romano <romano.romano@gmail.com>
2025-02-11 20:21:11 +0100

    feat: add Opt quote function

--- go-hello-world/main.go
+++ go-hello-world/main.go
@@ -15,9 +15,14 @@ func goQuoteHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, quote.Go())
 }
 
+func optQuoteHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, quote.Opt())
+}
+
 func main() {
 	http.HandleFunc("/", helloWorldHandler)
-	http.HandleFunc("/quote", quoteHandler)
+	http.HandleFunc("/go-quote", goQuoteHandler)
+	http.HandleFunc("/opt-quote", optQuoteHandler)
 
 	fmt.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
